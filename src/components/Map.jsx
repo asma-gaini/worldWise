74e185103d23ab832d6b887b9e06283f41a6bb16
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-04-03 14:19:50 +0330

    creat map (srollzoom,tilelayer org url b fr/hot and heght to map container)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,13 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 import styles from "./Map.module.css";
+import { useState } from "react";
 
 function Map() {
   //bara inke bdon inke link dashte bashim betunim ye seri etelaat b url befrestim
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   //khondane query ha mese state e taghribn e avalie dare ye tabe bara b roz resani
   const [searchParams, setSearchParams] = useSearchParams();
@@ -13,17 +17,22 @@ function Map() {
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position : {lat} , {lng}
-      </h1>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 23, lng: 50 });
-        }}
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Change pos
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
