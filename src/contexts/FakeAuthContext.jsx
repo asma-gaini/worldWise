22ff5fb8b3ff40caca424bc3f4f9ff2411d4a139
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-04-09 11:42:12 +0330

    cract context for authenticate user for login and logout with reducer

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,53 @@
+import { createContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = { user: null, isAthenticated: false };
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAthenticated: false };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function AuthProvider({ children }) {
+  const [{ user, isAthenticated }, dispatch] = useReducer(
+    reducer,
+    initialstate
+  );
+
+  //dar asl va vagheiyat inja ma b api darkhast midim va onja etebar sanji mishe vali aln faghat ba ye data fake
+  //k ma in etelate fake ru tu user component gharar dadim k copy mikonim
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("AuthContext was used outside of the AuthProvider");
+  //   return context;
+}
+export { AuthProvider, useAuth };
