5fb5f306e7462fc64e068d87a537713ce52abf95
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-04-09 13:31:55 +0330

    add user in app layloat

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,17 +2,10 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./User.module.css";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
 function User() {
-  const navigate = useNavigate();
+  const { user } = useAuth();
+  // const navigate = useNavigate();
 
-  const user = FAKE_USER;
   // const { isAthenticated, logout } = useAuth();
 
   function handleClick(e) {
@@ -20,11 +13,10 @@ function User() {
     // logout();
     // navigate("/");
   }
-
   return (
     <div className={styles.user}>
-      <img src={user.avatar} alt={user.name} />
-      <span>Welcome, {user.name}</span>
+      {/* <img src={user.avatar} alt={user.name} />
+      <span>Welcome, {user.name}</span> */}
       <button onClick={handleClick}>Logout</button>
     </div>
   );
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,12 +2,14 @@ import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
 
 import styles from "./AppLayout.module.css";
+import User from "../components/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
